0cb3044efd84591e0c2a0203a0fbcdf6d257fcf8
Katherine Eaton <katherine.eaton@phac-aspc.gc.ca>
2022-06-13 13:02:01 -0500

    add param --sc2rf-dir to allow execution outside of main directory

--- sc2rf.py
+++ sc2rf.py
@@ -76,8 +76,6 @@ def main():
     # ANSI / VT 100 codes in windows terminal
     # See https://stackoverflow.com/a/39675059/39946
     os.system('')
-
-    mappings = read_mappings('mapping.csv')
     
     parser = argparse.ArgumentParser(
         description='Analyse SARS-CoV-2 sequences for potential, unknown recombinant variants.', 
@@ -111,9 +109,13 @@ def main():
     parser.add_argument('--hide-progress', action='store_true', help="Don't show progress bars during long task.")
     parser.add_argument('--csvfile', type=argparse.FileType('w'), help="Path to write results in CSV format.")
     parser.add_argument('--ignore-shared-subs', action='store_true', help="Ignore substitutions that are shared between all parents.")
+    parser.add_argument('--sc2rf-dir', required=False, help="Path to the sc2rf directory.", default=".")  
 
     global args
     args = parser.parse_args()
+
+    mapping_path = os.path.join(args.sc2rf_dir, "mapping.csv")
+    mappings = read_mappings(mapping_path)    
 
     if args.ansi:
         dot_character = '.'
@@ -138,8 +140,11 @@ def main():
 
     global reference
     vprint("Reading reference genome, lineage definitions...")
-    reference = read_fasta('reference.fasta', None)['MN908947 (Wuhan-Hu-1/2019)']
-    all_examples = read_examples('virus_properties.json')
+    reference_path = os.path.join(args.sc2rf_dir, "reference.fasta")
+    reference = read_fasta(reference_path, None)['MN908947 (Wuhan-Hu-1/2019)']
+
+    virus_properties_path = os.path.join(args.sc2rf_dir, "virus_properties.json")
+    all_examples = read_examples(virus_properties_path)
 
     used_examples = []
     if 'all' in args.clades:
